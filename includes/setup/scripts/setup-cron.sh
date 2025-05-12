15afc11ca95488dba49332ace26a1390a2891d43
Ray <ray@innerwebblueprint.com>
2025-05-12 09:35:35 -0700

    setting up crons

--- includes/setup/scripts/setup-cron.sh
+++ includes/setup/scripts/setup-cron.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # includes/setup/scripts/setup-cron.sh
 
+CALL_MODULE=$MODULE
 MODULE="SETUP CRON"
 
 # Load environment and logging
@@ -21,25 +22,24 @@ CRON_FILE="/etc/crontabs/root"
 grep -q "SHELL=" "$CRON_FILE" || echo "SHELL=/bin/bash" >> "$CRON_FILE"
 grep -q "PATH=" "$CRON_FILE" || echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> "$CRON_FILE"
 
+# Trigger WordPress cron manually every 5 minutes
+grep -q "wp-cron.php" "$CRON_FILE" || echo "*/5 * * * * curl -fsSL -k -H \"Host: ${IWB_DOMAIN}\" https://127.0.0.1/wp-cron.php > /dev/null 2>&1" >> "$CRON_FILE"
+
 # Add backup jobs if not already present
-grep -q "iwb-backup.sh mail hourly" "$CRON_FILE" || echo "5 * * * * . /var/data/state/docker-env.sh && iwb-backup.sh mail hourly > /dev/null 2>&1" >> "$CRON_FILE"
+## PostfixAdmin
 grep -q "iwb-backup.sh postfix hourly" "$CRON_FILE" || echo "10 * * * * . /var/data/state/docker-env.sh && iwb-backup.sh postfix hourly > /dev/null 2>&1" >> "$CRON_FILE"
+## Mail
+grep -q "iwb-backup.sh mail hourly" "$CRON_FILE" || echo "5 * * * * . /var/data/state/docker-env.sh && iwb-backup.sh mail hourly > /dev/null 2>&1" >> "$CRON_FILE"
+## Wordpress
+grep -q "iwb-backup.sh wpdb hourly" "$CRON_FILE" || echo "15 * * * * . /var/data/state/docker-env.sh && iwb-backup.sh wpdb hourly > /dev/null 2>&1" >> "$CRON_FILE"
+grep -q "iwb-backup.sh wphtml daily" "$CRON_FILE" || echo "0 2 * * * . /var/data/state/docker-env.sh && iwb-backup.sh wphtml daily > /dev/null 2>&1" >> "$CRON_FILE"
+## n8n
+grep -q "iwb-backup.sh n8n hourly" "$CRON_FILE" || echo "1 * * * * . /var/data/state/docker-env.sh && iwb-backup.sh n8n hourly > /dev/null 2>&1" >> "$CRON_FILE"
 
 #grep -q "iwb-logtest.sh" "$CRON_FILE" || echo "* * * * * . /var/data/state/docker-env.sh && iwb-logtest.sh 2>&1" >> "$CRON_FILE"
 
-# # Ensure correct SHELL and PATH at top of crontab
-# grep -q "SHELL=" "$CRON_FILE" || echo "SHELL=/bin/bash" >> "$CRON_FILE"
-# grep -q "PATH=" "$CRON_FILE" || echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> "$CRON_FILE"
-
-# # Add backup jobs if not already present
-# grep -q "iwb-backup.sh mail hourly" "$CRON_FILE" || echo "5 * * * * . /var/data/state/docker-env.sh && iwb-backup.sh mail hourly >> /var/log/iwb-backup.log 2>&1" >> "$CRON_FILE"
-# grep -q "iwb-backup.sh postfix hourly" "$CRON_FILE" || echo "10 * * * * . /var/data/state/docker-env.sh && iwb-backup.sh postfix hourly >> /var/log/iwb-backup.log 2>&1" >> "$CRON_FILE"
-
-
 log "Cron jobs configured."
 
-
-
-
+MODULE=$CALL_MODULE
 (return 0 2>/dev/null) || exit 0
 
